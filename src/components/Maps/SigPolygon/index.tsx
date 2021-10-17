7a50f66d30c244582597446133e8d7e96ea4f5ae
oajh04 <cloudtubekr@naver.com>
2021-10-17 20:29:12 +0900

    ⚡️ :: sigPolygon 지도 네이밍 수정

--- src/components/Maps/SigPolygon/index.tsx
+++ src/components/Maps/SigPolygon/index.tsx
@@ -19,13 +19,26 @@ const SigPolygon = () => {
                 coordinate.push({lat: info[1], lng: info[0]})
             })
             let boroughName = data.properties.SIG_KOR_NM;
-            let cityName;
-
-            if(boroughName.indexOf('시') !== -1){
-                cityName = boroughName.split('시')[0] + '시'
-            }
-            else {
-                cityName = boroughName
+            let cityNo = data.properties.SIG_CD.substr(0,2);
+            let cityName
+            switch(cityNo){
+                case '11': cityName='서울특별시'; break;
+                case '26': cityName='부산광역시'; break;
+                case '27': cityName='대구광역시'; break;
+                case '28': cityName='인천광역시'; break;
+                case '29': cityName='광주광역시'; break;
+                case '30': cityName='대전광역시'; break;
+                case '31': cityName='울산광역시'; break;
+                case '36': cityName='세종특별자치시'; break;
+                case '41': cityName='경기도'; break;
+                case '42': cityName='강원도'; break;
+                case '43': cityName='충청북도'; break;
+                case '44': cityName='충청남도'; break;
+                case '45': cityName='전라북도'; break;
+                case '46': cityName='전라남도'; break;
+                case '47': cityName='경상북도'; break;
+                case '48': cityName='경상남도'; break;
+                case '50': cityName='제주특별자치시'; break;
             }
 
             area.push(
@@ -44,15 +57,34 @@ const SigPolygon = () => {
 
     return (
         <>
-            {areas.map((data, index) => (\
+            {areas.map((data, index) => (
+                data.cityName === '경기도' &&
                 <Polygon
-                    key={`area-${data.boroughName}`}
+                    key={`area-${data.boroughName}-${index}`}
                     path={data.path}
-                    fillColor={data.isMouseover ? "#09f" : "#fff"}
+                    fillColor={data.isMouseOver ? "#09f" : "#fff"}
                     strokeWeight={2}
                     strokeColor={"#004c80"}
                     strokeOpacity={0.8}
                     fillOpacity={0.7}
+                    // onMouseover={() =>
+                    //     setAreas((prev) => [
+                    //       ...prev.filter((_, i) => i !== index),
+                    //       {
+                    //         ...prev[index],
+                    //         isMouseOver: true,
+                    //       },
+                    //     ])
+                    //   }
+                    // onMouseout={() =>
+                    //     setAreas((prev) => [
+                    //         ...prev.filter((_, i) => i !== index),
+                    //         {
+                    //         ...prev[index],
+                    //         isMouseOver: false,
+                    //         },
+                    //     ])
+                    // }
                 />
             ))}
         </>
