7155eef352bf68c32ef9c99eea2515a01337138b
oajh04 <cloudtubekr@naver.com>
2021-10-17 20:30:44 +0900

    ⚡️ :: (#5) 확대할 때마다 지도 레벨 가져오기

--- src/components/Maps/index.tsx
+++ src/components/Maps/index.tsx
@@ -1,30 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Map, Polygon, CustomOverlayMap, MapInfoWindow } from 'react-kakao-maps-sdk'
 import { data as polygon } from '../../Data/CityData'
 import CityPolygon from './CityPolygon';
 import SigPolygon from './SigPolygon';
 
 const Maps = () => {
+    const [ level, setLevel ] = useState<any>(12)
+    console.log(level)
 
     return (
         <>
             <Map // 지도를 표시할 Container
-            id={`map`}
-            center={{
-            // 지도의 중심좌표
-            lat: 37.566826,
-            lng: 126.9786567,
-            }}
-            style={{
-            // 지도의 크기
-            width: "100%",
-            height: "100vh",
-            }}
-            level={12}
-        >
-            {/* <CityPolygon /> */}
-            <SigPolygon />
-        </Map>
+                id={`map`}
+                center={{
+                    lat: 37.566826,
+                    lng: 126.9786567,
+                }}
+                style={{
+                    width: "100%",
+                    height: "100vh",
+                }}
+                level={level}
+                onZoomChanged={(map) => setLevel(map.getLevel())}
+            >
+                {
+                    level < 9 ?
+                    null
+                    :   level > 10 ?
+                        <CityPolygon />
+                        : <SigPolygon />
+                }
+            </Map>
         </>
     );
 }
