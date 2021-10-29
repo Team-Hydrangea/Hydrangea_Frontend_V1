ae6ec0a92876fe9eb69057ae7f5e78de5b5a024e
kky04994 <kkyo8747@naver.com>
2021-10-29 17:57:32 +0900

    :gear: :: (#25) 실행하기 위해 수정됨

--- src/components/Maps/CityPolygon/index.tsx
+++ src/components/Maps/CityPolygon/index.tsx
@@ -1,81 +1,79 @@
 import React, { useEffect, useState } from 'react';
-import { data as cityPolygon } from '../../../data/cityData'
+import { data as cityPolygon } from '../../../Data/CityData';
 import { Polygon } from 'react-kakao-maps-sdk';
 
 interface ICityData {
-    circuitName: string,
-    isMouseOver: boolean,
-    center: number[],
-    path: {
-        lat: number;
-        lng: number;
-    }[]
+  circuitName: string;
+  isMouseOver: boolean;
+  center: number[];
+  path: {
+    lat: number;
+    lng: number;
+  }[];
 }
 
 const CityPolygon = () => {
-    const [ areas, setAreas ] = useState<ICityData[]>([])
-    
-    useEffect(() => {
-        let data = cityPolygon.features;
-        let area: any[] = []
+  const [areas, setAreas] = useState<ICityData[]>([]);
 
-        data.forEach((data: any) => {
-            let coordinate: { lat: number; lng: number; }[] = []
-            let eachLength = data.geometry.coordinates.map((x: string | any[]) => x.length)
-            let maxNum = Math.max(...eachLength);
-            let where = eachLength.indexOf(maxNum);
+  useEffect(() => {
+    let data = cityPolygon.features;
+    let area: any[] = [];
 
-            data.geometry.coordinates[where].forEach((info: any, index: string | number) => {
-                coordinate.push({lat: info[1], lng: info[0]})
-            })
-            area.push(
-                {
-                    circuitName: data.properties.CTP_KOR_NM, 
-                    isMouseOver: false,
-                    path: coordinate,
-                    center: data.properties.center
-                }
-            )
-        });
-        setAreas(area)
-    },[])
+    data.forEach((data: any) => {
+      let coordinate: { lat: number; lng: number }[] = [];
+      let eachLength = data.geometry.coordinates.map((x: string | any[]) => x.length);
+      let maxNum = Math.max(...eachLength);
+      let where = eachLength.indexOf(maxNum);
 
-    console.log(areas)
+      data.geometry.coordinates[where].forEach((info: any, index: string | number) => {
+        coordinate.push({ lat: info[1], lng: info[0] });
+      });
+      area.push({
+        circuitName: data.properties.CTP_KOR_NM,
+        isMouseOver: false,
+        path: coordinate,
+        center: data.properties.center,
+      });
+    });
+    setAreas(area);
+  }, []);
 
-    return (
-        <>
-            {areas.map((data, index) => (
-                <Polygon
-                    key={`area-${data.circuitName}`}
-                    path={data.path}
-                    fillColor={data.circuitName === '광주광역시' ? "rgba( 255, 255, 255, 0 )" : "#fff"}
-                    zIndex={data.circuitName === '광주광역시' ? 2 : 1}
-                    strokeWeight={2}
-                    strokeColor={"#004c80"}
-                    strokeOpacity={0.8}
-                    fillOpacity={0.7}
-                    onMouseover={() =>
-                        setAreas((prev) => [
-                          ...prev.filter((_, i) => i !== index),
-                          {
-                            ...prev[index],
-                            isMouseOver: true,
-                          },
-                        ])
-                      }
-                    onMouseout={() =>
-                        setAreas((prev) => [
-                            ...prev.filter((_, i) => i !== index),
-                            {
-                            ...prev[index],
-                            isMouseOver: false,
-                            },
-                        ])
-                    }
-                />
-            ))}
-        </>
-    );
-}
+  console.log(areas);
+
+  return (
+    <>
+      {areas.map((data, index) => (
+        <Polygon
+          key={`area-${data.circuitName}`}
+          path={data.path}
+          fillColor={data.circuitName === '광주광역시' ? 'rgba( 255, 255, 255, 0 )' : '#fff'}
+          zIndex={data.circuitName === '광주광역시' ? 2 : 1}
+          strokeWeight={2}
+          strokeColor={'#004c80'}
+          strokeOpacity={0.8}
+          fillOpacity={0.7}
+          onMouseover={() =>
+            setAreas(prev => [
+              ...prev.filter((_, i) => i !== index),
+              {
+                ...prev[index],
+                isMouseOver: true,
+              },
+            ])
+          }
+          onMouseout={() =>
+            setAreas(prev => [
+              ...prev.filter((_, i) => i !== index),
+              {
+                ...prev[index],
+                isMouseOver: false,
+              },
+            ])
+          }
+        />
+      ))}
+    </>
+  );
+};
 
 export default CityPolygon;
