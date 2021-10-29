8281932eb9c6f0f90f25aea380ad62b4ffcd6e34
hyejun <hyejun0608@naver.com>
2021-10-29 19:24:46 +0900

    ⚡️ :: (#11) 검색 박스 생성

---
+++ src/components/Location/Search.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import * as S from './style';
+
+interface Props {}
+
+const Search: FC<Props> = () => {
+  return <S.Search></S.Search>;
+};
+
+export default Search;
