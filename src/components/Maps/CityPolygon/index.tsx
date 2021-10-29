02eaf64d778c295791de13189a22c810be51f18e
oajh04 <cloudtubekr@naver.com>
2021-10-29 21:14:58 +0900

    ⚡️ :: (#34) 폴리곤 클릭시 이동 및 확대

--- src/components/Maps/CityPolygon/index.tsx
+++ src/components/Maps/CityPolygon/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { data as cityPolygon } from '../../../data/cityData'
+import { data as cityPolygon } from '../../../data/CityData'
 import { Polygon } from 'react-kakao-maps-sdk';
+import { mapsState } from '../../../recoil/mapsState';
+import { useSetRecoilState,useRecoilState } from 'recoil'
 
 interface ICityData {
     circuitName: string,
@@ -14,6 +16,7 @@ interface ICityData {
 
 const CityPolygon = () => {
     const [ areas, setAreas ] = useState<ICityData[]>([])
+    const [map, setMap] = useRecoilState(mapsState)
     
     useEffect(() => {
         let data = cityPolygon.features;
@@ -40,12 +43,32 @@ const CityPolygon = () => {
         setAreas(area)
     },[])
 
-    console.log(areas)
+    const onCenterMove = (name: string,center: number[]) => {
+        setMap({
+            cityName: name,
+            center: {
+                lat: center[0],
+                lng: center[1]
+            },
+            level: map.level !== 12 ? map.level : 12
+        })
+        map.cityName === name &&
+        setMap({
+            ...map,
+            center: {
+                lat: center[0],
+                lng: center[1]
+            },
+            level: map.cityName !== name ? map.level : map.level-1
+        })
+    }
+    console.log(map)
 
     return (
         <>
             {areas.map((data, index) => (
                 <Polygon
+                    onClick={() => onCenterMove(data.circuitName,data.center)}
                     key={`area-${data.circuitName}`}
                     path={data.path}
                     fillColor={data.circuitName === '광주광역시' ? "rgba( 255, 255, 255, 0 )" : "#fff"}
@@ -54,24 +77,24 @@ const CityPolygon = () => {
                     strokeColor={"#004c80"}
                     strokeOpacity={0.8}
                     fillOpacity={0.7}
-                    onMouseover={() =>
-                        setAreas((prev) => [
-                          ...prev.filter((_, i) => i !== index),
-                          {
-                            ...prev[index],
-                            isMouseOver: true,
-                          },
-                        ])
-                      }
-                    onMouseout={() =>
-                        setAreas((prev) => [
-                            ...prev.filter((_, i) => i !== index),
-                            {
-                            ...prev[index],
-                            isMouseOver: false,
-                            },
-                        ])
-                    }
+                    // onMouseover={() =>
+                    //     setAreas((prev) => [
+                    //       ...prev.filter((_, i) => i !== index),
+                    //       {
+                    //         ...prev[index],
+                    //         isMouseOver: true,
+                    //       },
+                    //     ])
+                    //   }
+                    // onMouseout={() =>
+                    //     setAreas((prev) => [
+                    //         ...prev.filter((_, i) => i !== index),
+                    //         {
+                    //         ...prev[index],
+                    //         isMouseOver: false,
+                    //         },
+                    //     ])
+                    // }
                 />
             ))}
         </>
