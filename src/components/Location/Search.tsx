227a02541b18eaf6c0a8d9136aba4efa9555f8b2
hyejun <hyejun0608@naver.com>
2021-10-29 19:41:28 +0900

    ⚡️ :: (#11) 검색 박스

--- src/components/Location/Search.tsx
+++ src/components/Location/Search.tsx
@@ -1,10 +1,16 @@
 import React, { FC } from 'react';
 import * as S from './style';
+import { searchIcon } from '../../assets/Search';
 
 interface Props {}
 
 const Search: FC<Props> = () => {
-  return <S.Search></S.Search>;
+  return (
+    <S.Search>
+      <S.SearchIcon src={searchIcon} />
+      <S.SearchInput placeholder={'장소, 주소를 검색하세요.'} />
+    </S.Search>
+  );
 };
 
 export default Search;
--- src/components/Location/style/index.ts
+++ src/components/Location/style/index.ts
@@ -7,6 +7,10 @@ export const Location = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
 `;
 
 export const Random = styled.div`
@@ -29,4 +33,35 @@ export const Title = styled.p`
 export const SubTitle = styled.p`
   font-size: 11px;
   margin-top: 3px;
+`;
+
+export const Search = styled.div`
+  width: 307px;
+  height: 42px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  margin: 23px 0px 10px 0px;
+  padding: 0px 16px;
+`;
+
+export const SearchIcon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+export const SearchInput = styled.input`
+  width: 250px;
+  height: 100%;
+  font-size: 14px;
+  padding: 0px 10px;
+  border: none;
+  :placeholder-shown {
+    font-size: 13px;
+    font-weight: bold;
+    color: rgba(0, 0, 0, 0.5);
+  }
 `;
