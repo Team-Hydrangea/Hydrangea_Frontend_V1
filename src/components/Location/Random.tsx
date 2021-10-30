5992695b80c4afaab216e765fd8414799f88b8a1
hyejun <hyejun0608@naver.com>
2021-10-30 20:57:27 +0900

    ⚡️ :: (#44) 즐겨찾기가 있으면 즐겨찾기를, 없으면 랜덤 지역을 불러오는 api 호출

--- src/components/Location/Random.tsx
+++ src/components/Location/Random.tsx
@@ -1,4 +1,7 @@
-import React, { FC, useEffect, useState } from 'react';
+import axios from 'axios';
+import React, { FC, useEffect, useMemo, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { bookmarkRandomState, randomState } from '../../recoil/randomState';
 import * as S from './style';
 
 interface Props {
@@ -6,12 +9,31 @@ interface Props {
 }
 
 const Random: FC<Props> = props => {
+  const [random, setRandom] = useRecoilState(randomState);
+  const [bookmarkRandom, setBookmarkRandom] = useRecoilState(bookmarkRandomState);
   const { isHaveBookMark } = props;
   const [content, setContent] = useState('');
 
   useEffect(() => {
     if (isHaveBookMark) setContent('즐겨찾기 장소 랜덤 추천 🎉');
     else setContent('랜덤 관광지 추천 🎉');
+  }, [isHaveBookMark]);
+
+  useEffect(() => {
+    if (isHaveBookMark)
+      axios
+        .get(`${process.env.REACT_APP_BASE_URL}bookmark/random`, {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+          },
+        })
+        .then(response => setBookmarkRandom(response.data))
+        .catch(error => console.log(error));
+    else
+      axios
+        .get(`${process.env.REACT_APP_BASE_URL}region/safe`)
+        .then(response => setRandom({ name: response.data.name }))
+        .catch(error => console.log(error));
   }, [isHaveBookMark]);
 
   return (
---
+++ src/recoil/randomState.ts
@@ -0,0 +1,21 @@
+import { atom } from 'recoil';
+
+export const randomState = atom({
+  key: 'randomState',
+  default: {
+    name: '',
+  },
+});
+
+export const bookmarkRandomState = atom({
+  key: 'bookmarkRandomState',
+  default: {
+    address: '',
+    detail_address: '',
+    image: '',
+    latitude: 0,
+    longitude: 0,
+    number: '',
+    title: '',
+  },
+});
