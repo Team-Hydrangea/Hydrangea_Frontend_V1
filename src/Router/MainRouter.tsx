d00f2a92f2f5fb36f7f3edbbdae2cafd25e401be
kky04994 <kkyo8747@naver.com>
2021-10-30 16:08:19 +0900

    :bookmark_tabs: :: (#25) 구조 변경됨

--- src/Router/MainRouter.tsx
+++ src/Router/MainRouter.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
-import { 
-  Main,
- } from '../components';
+import { Main } from '../components';
 
 const MainRouter = () => {
   const location = useLocation();
 
   return (
     <>
-        <Switch>
-          <Route path='/' component={Main} exact/>
-        </Switch>
+      <Switch>
+        <Route path='/' component={Main} exact />
+      </Switch>
     </>
   );
-}
+};
 
 export default MainRouter;
