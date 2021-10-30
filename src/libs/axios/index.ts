35238dce5a8216df709414e06f659f782699eb37
hyejun <hyejun0608@naver.com>
2021-10-31 06:53:38 +0900

    ⚡️ :: (#59) 리프레시 토큰

--- src/libs/axios/index.ts
+++ src/libs/axios/index.ts
@@ -1,24 +1,26 @@
-import axios, {AxiosError} from 'axios';
+import axios, { AxiosError } from 'axios';
+import { refreshToken } from '../api/refreshToken';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    timeout: 10000
+  baseURL: process.env.REACT_APP_BASE_URL,
+  timeout: 10000,
 });
 instance.interceptors.request.use(
-    function (config) {
-        return config;
-    }, 
-    function (error : AxiosError) {
-        return Promise.reject(error);
-    }
+  function (config) {
+    return config;
+  },
+  function (error: AxiosError) {
+    return Promise.reject(error);
+  },
 );
 instance.interceptors.response.use(
-    function (response) {
-        return response;
-    },
+  function (response) {
+    return response;
+  },
 
-    function (error : AxiosError) {
-        return Promise.reject(error);
-    }
+  function (error: AxiosError) {
+    if (error.response?.status === 401 || error.response?.status === 403) refreshToken();
+    return Promise.reject(error);
+  },
 );
 export default instance;
