eb90617d018ccacec98eab631b26fa6cc47ee2a1
kky04994 <kkyo8747@naver.com>
2021-10-30 16:06:32 +0900

    ⚡️ :: (#25) LogOut 생성

---
+++ src/components/BookMark/LogOut.tsx
@@ -0,0 +1,12 @@
+import * as S from './style';
+import React from 'react';
+
+const LogOut = () => {
+  return (
+    <S.LogOutBox>
+      <p>로그아웃</p>
+    </S.LogOutBox>
+  );
+};
+
+export default LogOut;
