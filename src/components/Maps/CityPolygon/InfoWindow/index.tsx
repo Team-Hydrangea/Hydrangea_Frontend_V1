3f740559c06c55991368f4abdef8e5db93afeb0b
oajh04 <cloudtubekr@naver.com>
2021-10-30 23:13:23 +0900

    ⚡️ :: (#64) custom overay infowindow

--- src/components/Maps/CityPolygon/InfoWindow/index.tsx
+++ src/components/Maps/CityPolygon/InfoWindow/index.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
-import { MapInfoWindow } from 'react-kakao-maps-sdk';
+import { CustomOverlayMap, MapInfoWindow, MapMarker } from 'react-kakao-maps-sdk';
 import { useRecoilState } from 'recoil'
-import { mapsState, mousePositionState } from '../../../../recoil/mapsState';
+import { mousePositionState } from '../../../../recoil/mapsState';
+import * as S from './styles'
+import {AiOutlineClose} from 'react-icons/ai'
 
 interface Props {
   value: any[]
@@ -11,31 +13,22 @@ const InfoWindow:FC<Props> = ({value}) => {
   const [ mapData, setMapData ] = useRecoilState(mousePositionState)
   
   const infoData = value.filter((i) => i.name === mapData.name)[0];
-  console.log(infoData)
 
   return (
     <>
-        <MapInfoWindow position={{lat: mapData.lat, lng: mapData.lng}}>
-            <img
-              alt="close"
-              width="14"
-              height="13"
-              src="http://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
-              style={{
-                position: "absolute",
-                right: "5px",
-                top: "5px",
-                cursor: "pointer",
-              }}
-              onClick={() => setMapData({lat: 0, lng:0, name: ''})}
-            ></img>
-            <div className="info">
-              <div className="title">{infoData.name}</div>
-              <div>총 인구 : {infoData.population}</div>
-              <div>확진자수 : {infoData.confirm_case_count}</div>
-              <div>백신 접종수 : {infoData.vaccinate_case_count}</div>
-            </div>
-          </MapInfoWindow>
+        <CustomOverlayMap position={{lat: mapData.lat, lng: mapData.lng}}>
+            <S.InfoWindowWrapper>
+              <S.InfoWindowHeader>
+                {infoData.name}
+                <AiOutlineClose onClick={() => setMapData({lat: 0, lng:0, name: ''})}/>
+              </S.InfoWindowHeader>
+              <S.InfoWindowBody>
+                <p>확진자 : {infoData.confirm_case_count} 명</p>
+                <p>접종자 : {infoData.vaccinate_case_count} 명</p>
+                <p>{infoData.score.toFixed(1)} 점</p>
+              </S.InfoWindowBody>
+            </S.InfoWindowWrapper>
+          </CustomOverlayMap>
     </>
   );
 }
