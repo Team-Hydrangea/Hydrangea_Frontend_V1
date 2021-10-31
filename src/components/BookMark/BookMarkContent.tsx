570f1864c85c5ce124debf3343a8e514d5599dab
kky04994 <kkyo8747@naver.com>
2021-10-31 09:09:22 +0900

    ⚡️ :: (#41) 모달 추가

--- src/components/BookMark/BookMarkContent.tsx
+++ src/components/BookMark/BookMarkContent.tsx
@@ -1,8 +1,10 @@
 import * as S from './style';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import * as BsIcons from 'react-icons/bs';
+import StarScore from './StarScore';
 
 interface Props {
+  setIsOpenModal?: React.Dispatch<React.SetStateAction<boolean>>;
   address: string;
   title: string;
   latitude: number;
@@ -12,21 +14,28 @@ interface Props {
 }
 
 const BookMarkContent: FC<Props> = props => {
-  const { address, title, page } = props;
-  const onClickText = () => {};
+  const [isModal, setIsModal] = useState(false);
+  const { address, title, latitude, longitude } = props;
+  const onClickText = () => {
+    setIsModal(!isModal);
+  };
+
   return (
-    <S.BookMarkContentBox>
-      <S.Content>
-        <S.TopBox>
-          <BsIcons.BsStarFill color='#7184d6' />
-          <S.Title>{title}</S.Title>
-        </S.TopBox>
-        <S.BottomBox>
-          <S.AddressText>{address}</S.AddressText>
-          <S.StartText onClick={onClickText}>별점확인</S.StartText>
-        </S.BottomBox>
-      </S.Content>
-    </S.BookMarkContentBox>
+    <>
+      {isModal && <StarScore setIsModal={setIsModal} lat={latitude} lng={longitude} />}
+      <S.BookMarkContentBox>
+        <S.Content>
+          <S.TopBox>
+            <BsIcons.BsStarFill color='#7184d6' />
+            <S.Title>{title}</S.Title>
+          </S.TopBox>
+          <S.BottomBox>
+            <S.AddressText>{address}</S.AddressText>
+            <S.StarText onClick={onClickText}>별점확인</S.StarText>
+          </S.BottomBox>
+        </S.Content>
+      </S.BookMarkContentBox>
+    </>
   );
 };
 
